10caab1042e5f6770e7511cbdc0a61540d110bed
Development for People <47140294+developmentforpeople@users.noreply.github.com>
2023-01-28 09:29:48 +0000

    TODO: tests todos

--- dfp_external_storage/dfp_external_storage/doctype/dfp_external_storage/test_dfp_external_storage.py
+++ dfp_external_storage/dfp_external_storage/doctype/dfp_external_storage/test_dfp_external_storage.py
@@ -6,4 +6,9 @@ from frappe.tests.utils import FrappeTestCase
 
 
 class TestDFPExternalStorage(FrappeTestCase):
+	# TODO: create DFP External Storage
+	# TODO: upload file to bucket
+	# TODO: read bucket file
+	# TODO: relocate bucket file
+	# TODO: delete bucket file
 	pass
