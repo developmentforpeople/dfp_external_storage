d89e65acb68dab7eb130b800b555d79c957f87d7
ahmedsamanmohammed <ahmed.saman@alsharaa-dent.com>
2025-12-09 13:39:31 +0000

    feat(api.py):add create file api

--- dfp_external_storage/api.py
+++ dfp_external_storage/api.py
@@ -28,3 +28,46 @@ def generate_presigned_url(storage_name, file_name, file_path="Record"):
         "get_url": get_url,
         "s3_key": s3_key,
     }
+
+@frappe.whitelist()
+def create_file_record(
+    file_name,
+    file_size,
+    s3_key,
+    storage_name,
+    attached_to_doctype=None,
+    attached_to_name=None,
+    attached_to_field=None,
+    is_private=1,
+    folder="Home",
+    **kwargs,
+):
+    file_doc = frappe.get_doc(
+        {
+            "doctype": "File",
+            "file_name": file_name,
+            "file_size": file_size,
+            "dfp_external_storage": storage_name,
+            "dfp_external_storage_s3_key": s3_key,
+            "attached_to_doctype": attached_to_doctype,
+            "attached_to_name": attached_to_name,
+            "attached_to_field": attached_to_field,
+            "folder": folder,
+            "is_private": is_private,
+            **kwargs,
+        }
+    )
+
+    file_doc.insert(ignore_permissions=True)
+
+    file_url = f"/file/{file_doc.name}/{file_name}"
+    file_doc.update({"file_url": file_url})
+    file_doc.save(ignore_permissions=True)
+
+    if attached_to_doctype and attached_to_name and attached_to_field:
+        parent_doc = frappe.get_doc(attached_to_doctype, attached_to_name)
+        parent_doc.update({attached_to_field: file_url})
+        parent_doc.save(ignore_permissions=True)
+    frappe.db.commit()
+
+    return {"file_doc": file_doc, "file_url": file_url}
